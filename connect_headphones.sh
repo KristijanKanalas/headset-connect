918a33319f9a896690da34eee07810d43867347e
Kristijan Kanalaš <kanalaskristijan@gmail.com>
2019-09-19 16:00:00 +0200

    Update comments

--- connect_headphones.sh
+++ connect_headphones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service bluetooth restart # Restart bluetooth
-sleep 3s # wait if for restart to finish
+sleep 3s # wait for restart to finish
 <<<"trust {MAC_ADDR}
 connect {MAC_ADDR}
 quit" bluetoothctl # connect headset
